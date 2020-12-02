70173d5043cf1e589a19309a3b447f2dd15cebdc
richards <richards@DESKTOP-BDIH07R>
2020-12-02 14:37:47 -0400

    2-12-2020

--- src/java/controlador/proceso/proceso.java
+++ src/java/controlador/proceso/proceso.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import proyecto.Usuarios;
-
+import proyecto.PruebaPersona;
+@SessionAttributes({"pruebapersona"})
 /**
  *
  * @author richards
@@ -20,11 +22,12 @@ import proyecto.Usuarios;
 @RequestMapping("imiproceso.htm")
 public class proceso {
    @RequestMapping(method=RequestMethod.GET)
-    public ModelAndView principalControlerGet() 
+    public ModelAndView principalControlerGet(@ModelAttribute("pruebapersona") PruebaPersona pruebapersona ) 
             
     {   
         
-        
+        System.out.println("Los datos son:");
+        System.out.print(pruebapersona.getCi()+"  "+pruebapersona.getNombre()+" "+pruebapersona.getEdad());
         System.out.println("Llego metodo GET");
         ModelAndView mav =new ModelAndView();
         mav.setViewName("imiproceso");
